265886e050d572adbd9e49059af4d0d8b69073a8
ashiqmnb <ashiqamohammed@gmail.com>
2025-03-29 15:01:15 +0530

    logout updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,20 @@
 import { Box, Tooltip, Typography } from "@mui/material";
 import { TbCoinRupeeFilled } from "react-icons/tb";
-import NotificationsIcon from "@mui/icons-material/Notifications";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { useNavigate } from "react-router-dom";
+import LogoutModal from "./modals/LogoutModal";
+import { useState } from "react";
 
 const Navbar = () => {
    const navigate = useNavigate();
 
    const coins = localStorage.getItem("coins");
+   const [openLogout, setOpenLogout] = useState<boolean>(false);
+
+   const handleLogout = ()=>{
+      localStorage.clear();
+      navigate("/login");
+   }
 
    return (
       <Box
@@ -16,7 +23,7 @@ const Navbar = () => {
             width: "auto",
             backgroundColor: "#2DC98A",
          }}
-      >
+         >
          <Box
             sx={{
                height: "100%",
@@ -26,39 +33,39 @@ const Navbar = () => {
                alignItems: "center",
             }}
          >
-         <Typography
-            sx={{
-               fontFamily: "Libre Baskerville",
-               fontSize: "clamp(14px, 2vw, 24px)",
-               fontWeight: "600",
-            }}
-         >
-            Admin Panal
-         </Typography>
-         <Box sx={{ display: "flex", alignItems: "center", gap: 2, }}>
-            <Box sx={{ display: "flex", alignItems: "center", gap: 0.5, }} >
-               <Typography sx={{ fontWeight: "600" }}>{coins}</Typography>
-               <TbCoinRupeeFilled
-                  style={{
-                     color: "#FFD700",
-                     fontSize: "25px",
-                  }}
-               />
-            </Box>
-            <Tooltip title="notifications" arrow>
-               <NotificationsIcon sx={{ cursor: "pointer" }} />
-            </Tooltip>
-            <Tooltip title="log out" arrow>
-               <LogoutIcon
-               onClick={() => {
-                  localStorage.clear();
-                  navigate("/login");
+            <Typography
+               sx={{
+                  fontFamily: "Libre Baskerville",
+                  fontSize: "clamp(14px, 2vw, 24px)",
+                  fontWeight: "600",
                }}
-               sx={{ cursor: "pointer" }}
-               />
-            </Tooltip>
+            >
+               Admin Panal
+            </Typography>
+            <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
+               <Box sx={{ display: "flex", alignItems: "center", gap: 0.5, }} >
+                  <Typography sx={{ fontWeight: "600" }}>{coins}</Typography>
+                  <TbCoinRupeeFilled
+                     style={{
+                        color: "#FFD700",
+                        fontSize: "25px",
+                     }}
+                  />
+               </Box>
+               <Tooltip title="log out" arrow>
+                  <LogoutIcon 
+                     onClick={()=> setOpenLogout(true)}
+                     sx={{ cursor: "pointer" }}
+                  />
+               </Tooltip>
+            </Box>
          </Box>
-         </Box>
+         
+         <LogoutModal
+            open={openLogout}
+            onClose={()=> setOpenLogout(false)}
+            onConfirm={handleLogout}
+         />
       </Box>
    );
 };
